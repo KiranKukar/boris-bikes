6442ea41cb9a18ff2a827adabfeb68ff1f6c58e1
KiranKukar <kiranrkukar@gmail.com>
2021-12-10 17:18:55 +0000

    wrote test for error when no bikes in dock, and wrote code for it

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,6 +8,8 @@ describe DockingStation do
     describe 'release_bike' do 
       it 'releases a bike' do
         station = DockingStation.new
+        bike = Bike.new
+        station.dock(bike)
         expect(station.release_bike).to be_instance_of(Bike)
       end
     end
@@ -29,4 +31,10 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
+
+    it "raises an error when dock is empty and release_bike is called" do
+      station = DockingStation.new
+      expect { station.release_bike }.to raise_error("There are no bikes")
+    end
+    
 end
