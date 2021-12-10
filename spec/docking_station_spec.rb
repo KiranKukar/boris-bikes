be7d1998a0896cad5b3503a62bdae8c5a93d821e
KiranKukar <kiranrkukar@gmail.com>
2021-12-10 14:18:00 +0000

    add dock and attr_reader

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,32 @@
-
 require 'docking_station_class'
-require 'bike_class'
 
 describe DockingStation do
-    it { is_expected.to respond_to(:release_bike) }
+    describe 'release_bike' do 
+      it { is_expected.to respond_to :release_bike } 
+    end
 
-    describe '#release_bike' do
-        it { should be_a(Bike) } #this is close, I need to add something that calls release_bike to create the Bike objetc first
+    describe 'release_bike' do 
+      it 'releases a bike' do
+        station = DockingStation.new
+        expect(station.release_bike).to be_instance_of(Bike)
+      end
+    end
+
+    describe 'working?' do
+      it 'pull bike and check to see if working' do
+      bike = Bike.new
+      expect(bike.working?).to eq true
+      end
+    end
+
+    it 'docks an argument when called to' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns a docked bike when called to' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
     end
 end
